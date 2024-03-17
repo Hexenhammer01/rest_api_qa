95fd77fbd422835529703c00966e4e05b1b00b45
Hexenhammer01 <railxpp@gmail.com>
2024-03-17 22:38:06 +0300

    парсинг json

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+from json.decoder import JSONDecodeError
 import requests
-payload = {"name" : "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params= payload)
+
+# payload = {"name" : "User"}
+# response = requests.get("https://playground.learnqa.ru/api/hello", params= payload)
+# print(response.text)
+
+# response = requests.get("https://playground.learnqa.ru/api/hello", params= {"name" : "User"})
+# parsed_response_text = response.json( )
+# print(parsed_response_text["answer"])
+
+#
+# response = requests.get("https://playground.learnqa.ru/api/get_text")
+# print(response.text)
+#
+# try:
+#     parsed_response_text = response.json()
+#     print(parsed_response_text)
+# except JSONDecodeError:
+#     print("response is not JSON format")
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text)
+except JSONDecodeError:
+    print("Respons is not JSON");
+
